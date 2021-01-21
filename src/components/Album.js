b6ba725f56c47f0715a3b650ef164da414068949
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2021-01-21 14:15:13 +0545

    Initialized Project

---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Album = () => {
+    return (
+        <div className="">
+            .
+        </div>
+    )
+}
+
+export default Album
---
+++ src/components/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from './Album';
+
+const Player = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Player
