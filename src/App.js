f06c0cc467dd7d727bf07f156e0a5e324da8f57f
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2021-01-22 11:57:40 +0545

    Created components required for showing FM stations list.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import StationManager from './components/StationManager';
+import PlayManager from './components/PlayManager';
+// Styled component
+import styled from 'styled-components';
+// Global styles
+import GlobalStyle from './components/GlobalStyles';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<MainDiv>
+				<StyledStationManager />
+				<PlayManager />
+			</MainDiv>
+		</>
+	);
 }
+
+const MainDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 100vh;
+`;
+
+const StyledStationManager = styled(StationManager)`
+	width: 30%;
+`;
 
 export default App;
