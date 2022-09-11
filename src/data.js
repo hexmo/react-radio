7a814dce366f964d6c764df7a477a58fabe2cf2b
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-09-11 13:12:15 +0545

    feat(station list): only use https stations

--- src/data.js
+++ src/data.js
@@ -1,76 +1,72 @@
 function radio() {
-	return [
-		{
-			id: 0,
-			name: 'Radio Kantipur',
-			logo: 'https://radio-jcss-cdn.ekantipur.com/images/kfm_logo.png',
-			audio: 'http://broadcast.radiokantipur.com:7248/stream/',
-			active: true,
-		},
-		{
-			id: 1,
-			name: 'Kalika FM',
-			logo: 'https://cdn-radiotime-logos.tunein.com/s48026g.png',
-			audio: 'http://kalika-stream.softnep.com:7740/;',
-			active: false,
-		},
-		{
-			id: 2,
-			name: 'Radio Nepal',
-			logo: 'https://images.hamro-files.com/Dn7WXDJ4gUldD71C1tjYlg09IPc=/500x500/https://sgp1.digitaloceanspaces.com/everestdb/hamropatro-backend/radio/7b57852c-4a14-4695-8dc5-9879f6be8780',
-			audio: 'http://103.69.126.230:8000/stream',
-			active: false,
-		},
-		{
-			id: 3,
-			name: 'Ujyaalo 90 Network',
-			logo: 'https://static-media.streema.com/media/cache/de/39/de395c2cef8231ce24009ace8d2c63a7.jpg',
-			audio: 'https://stream.zeno.fm/h527zwd11uquv',
-			active: false,
-		},
-		{
-			id: 4,
-			name: 'Radio Audio',
-			logo: 'https://cdn-profiles.tunein.com/s105813/images/logog.png',
-			audio: 'https://stream.zeno.fm/fvrx47wpg0quv',
-			active: false,
-		},
-		{
-			id: 5,
-			name: 'Radio Sargam',
-			logo: 'https://mytuner.global.ssl.fastly.net/media/tvos_radios/vkaf8ldacxcw.png',
-			audio: 'http://rstream.abgroupnepal.com:8000/radiosargam',
-			active: false,
-		},
-		{
-			id: 6,
-			name: 'Radio Annapurna Nepal',
-			logo: 'https://cdn-radiotime-logos.tunein.com/s180463q.png',
-			audio: 'http://streaming.softnep.net:8091/;',
-			active: false,
-		},
-		{
-			id: 7,
-			name: 'Capital FM',
-			logo: 'https://www.cfmonair.com/uploads/images/161025986597c56cf24e250e6a1799ae4b285581.jpg',
-			audio: 'http://streaming.softnep.net:8037/;',
-			active: false,
-		},
-		{
-			id: 8,
-			name: 'Hits FM',
-			logo: 'https://static-media.streema.com/media/cache/a5/d8/a5d827f31d007b629c059f19f35cb59b.jpg',
-			audio: 'https://usa15.fastcast4u.com/proxy/hitsfm912?mp=/1',
-			active: false,
-		},
-		{
-			id: 9,
-			name: 'Mithilanchal FM',
-			logo: 'https://dbs.radioline.fr/pictures/radio_04646c9dbf216aede926c85722705814/logo200.jpg?size=200',
-			audio: 'https://live.itech.host:8645/stream',
-			active: false,
-		},
-	];
+  return [
+    {
+      id: 0,
+      name: "Radio Kantipur",
+      logo: "https://radio-jcss-cdn.ekantipur.com/images/kfm_logo.png",
+      audio: "https://radio-broadcast.ekantipur.com/stream",
+      active: true,
+    },
+    {
+      id: 1,
+      name: "Kalika FM",
+      logo: "https://cdn-radiotime-logos.tunein.com/s48026g.png",
+      audio:
+        "https://streaming.softnep.net:10828/stream?type=http&nocache=3662",
+      active: false,
+    },
+    {
+      id: 2,
+      name: "Radio Nepal",
+      logo: "https://images.hamro-files.com/Dn7WXDJ4gUldD71C1tjYlg09IPc=/500x500/https://sgp1.digitaloceanspaces.com/everestdb/hamropatro-backend/radio/7b57852c-4a14-4695-8dc5-9879f6be8780",
+      audio: "https://radionepal.news/live",
+      active: false,
+    },
+    {
+      id: 3,
+      name: "Ujyaalo 90 Network",
+      logo: "https://static-media.streema.com/media/cache/de/39/de395c2cef8231ce24009ace8d2c63a7.jpg",
+      audio: "https://stream.zeno.fm/h527zwd11uquv",
+      active: false,
+    },
+    {
+      id: 4,
+      name: "Radio Audio",
+      logo: "https://cdn-profiles.tunein.com/s105813/images/logog.png",
+      audio: "https://stream.zeno.fm/fvrx47wpg0quv",
+      active: false,
+    },
+    {
+      id: 5,
+      name: "Radio Annapurna Nepal",
+      logo: "https://cdn-radiotime-logos.tunein.com/s180463q.png",
+      audio:
+        "https://streaming.softnep.net:10980/stream?type=http&nocache=214122",
+      active: false,
+    },
+    {
+      id: 6,
+      name: "Capital FM",
+      logo: "https://www.cfmonair.com/uploads/images/161025986597c56cf24e250e6a1799ae4b285581.jpg",
+      audio:
+        "https://streaming.softnep.net:10982/;stream.nsv&type=mp3&volume=10",
+      active: false,
+    },
+    {
+      id: 7,
+      name: "Hits FM",
+      logo: "https://static-media.streema.com/media/cache/a5/d8/a5d827f31d007b629c059f19f35cb59b.jpg",
+      audio: "https://usa15.fastcast4u.com/proxy/hitsfm912?mp=/1",
+      active: false,
+    },
+    {
+      id: 8,
+      name: "Mithilanchal FM",
+      logo: "https://dbs.radioline.fr/pictures/radio_04646c9dbf216aede926c85722705814/logo200.jpg?size=200",
+      audio: "https://live.itech.host:8645/stream",
+      active: false,
+    },
+  ];
 }
 
 export default radio;
